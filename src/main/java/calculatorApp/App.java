040aa8d3876afeeb556e71d6a564db713c111048
szakicsabi <csaba.szakacs.87@hotmail.com>
2023-11-08 17:00:17 +0100

    resize

--- src/main/java/calculatorApp/App.java
+++ src/main/java/calculatorApp/App.java
@@ -15,8 +15,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 750, 400);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
